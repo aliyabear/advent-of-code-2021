a798165e6b1544a6a941d86b5303585335267e9f
Aliya Hassam <aliya.hassam@gmail.com>
2021-12-04 00:04:02 -0800

    Day 3 - part 2

--- src/03/binary_diagnostics.ts
+++ src/03/binary_diagnostics.ts
@@ -27,3 +27,92 @@ export const getPowerConsumption = (input: string[]): number => {
 
   return parseInt(gamma.join(""), 2) * parseInt(epsilon.join(""), 2);
 };
+
+const getOxygenRating = (input: string[]): number => {
+  // return getRating(input, (bits: number, total: number, items: string[]) => {});
+  // number of values in input
+  let count = input.length;
+
+  if (count === 0) {
+    return 0;
+  }
+
+  // assume all values are the same length
+  const size = input[0].length;
+
+  // in the beginning, nothing is filtered!
+  let filteredForOxygenRating: string[] = [...input];
+  for (let i = 0; i < size; i++) {
+    count = filteredForOxygenRating.length;
+
+    if (count === 1) {
+      // return this number if only one is left
+      break;
+    }
+
+    const bits = filteredForOxygenRating
+      .map((item) => Number(item[i]))
+      .reduce((acc: number, curr: number) => acc + curr, 0);
+
+    if (bits >= Math.ceil(Number(count / 2))) {
+      // if we have an equal amount of 1s and 0s, use 1
+      // 1 is more common
+      filteredForOxygenRating = filteredForOxygenRating.filter(
+        (o) => o[i] === "1"
+      );
+    } else {
+      // 0 is more common
+      filteredForOxygenRating = filteredForOxygenRating.filter(
+        (o) => o[i] === "0"
+      );
+    }
+  }
+
+  return parseInt(filteredForOxygenRating[0], 2);
+};
+
+const getC02ScrubberRating = (input: string[]): number => {
+  // number of values in input
+  let count = input.length;
+
+  if (count === 0) {
+    return 0;
+  }
+
+  // assume all values are the same length
+  const size = input[0].length;
+
+  // in the beginning, nothing is filtered!
+  let filtered: string[] = [...input];
+  for (let i = 0; i < size; i++) {
+    count = filtered.length;
+
+    if (count === 1) {
+      // return this number if only one is left
+      break;
+    }
+
+    const bits = filtered
+      .map((item) => Number(item[i]))
+      .reduce((acc: number, curr: number) => acc + curr, 0);
+
+    if (bits >= Math.ceil(Number(count / 2))) {
+      // if we have an equal amount of 1s and 0s, use 1
+      // 1 is more common
+      filtered = filtered.filter((o) => o[i] === "0");
+    } else {
+      // 0 is more common
+      filtered = filtered.filter((o) => o[i] === "1");
+    }
+  }
+
+  return parseInt(filtered[0], 2);
+};
+
+// multiplying the oxygen generator rating by the CO2 scrubber rating.
+export const getLifeSupportRating = (input: string[]): number => {
+  const oxygenRating = getOxygenRating(input);
+  const c02ScrubberRating = getC02ScrubberRating(input);
+
+  return oxygenRating * c02ScrubberRating;
+};
--- tests/03/binary_diagnostics.test.ts
+++ tests/03/binary_diagnostics.test.ts
@@ -31,3 +31,35 @@ describe("getPowerConsumption", () => {
     expect(result).toBeUndefined();
   });
 });
+
+describe("getLifeSupportRating", () => {
+  test("returns 230 for given input", () => {
+    const input = [
+      "00100",
+      "11110",
+      "10110",
+      "10111",
+      "10101",
+      "01111",
+      "00111",
+      "11100",
+      "10000",
+      "11001",
+      "00010",
+      "01010",
+    ];
+
+    const result = sut.getLifeSupportRating(input);
+
+    expect(result).toEqual(230);
+  });
+
+  test("let's see what happens with sample input", () => {
+    const input = fs.readFileSync("./tests/03/input.txt", "utf8").split("\n");
+
+    const result = sut.getLifeSupportRating(input);
+
+    // this is done so we can see the answer without showing the answer ;-)
+    expect(result).toBeUndefined();
+  });
+});
